d47617dd4f5bb09a253c574c1bd21ff8bafa8d75
nayaraperez <nayaraperez@liceovvh.cl>
2026-05-14 13:00:47 -0400

    ok

---
+++ STACK_JS/static/js/script.js
@@ -0,0 +1,13 @@
+console.log("conexión de js exitosa...");
+
+function cambiarTexto() {
+    let titulo = "Hola, Bienvenido"
+    if (document.getElementById("titulo").innerText == titulo) {
+        document.getElementById("titulo").innerText = "Has cambiado el texto con JS";
+        document.getElementById("titulo").style.color="red"
+    } else {
+        document.getElementById("titulo").innerText = titulo;
+        document.getElementById("titulo").style.color="black"
+    }
+
+}
