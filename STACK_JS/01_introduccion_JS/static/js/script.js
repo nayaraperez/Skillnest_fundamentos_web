04dcc706e77c33f8c3a9882322d45f40c4e73373
nayaraperez <nayaraperez@liceovvh.cl>
2026-05-20 16:10:45 -0400

    ok

--- STACK_JS/01_introduccion_JS/static/js/script.js
+++ STACK_JS/01_introduccion_JS/static/js/script.js
@@ -4,10 +4,10 @@ function cambiarTexto() {
     let titulo = "Hola, Bienvenido"
     if (document.getElementById("titulo").innerText == titulo) {
         document.getElementById("titulo").innerText = "Has cambiado el texto con JS";
-        document.getElementById("titulo").style.color="red"
+        document.getElementById("titulo").style.color = "red"
     } else {
         document.getElementById("titulo").innerText = titulo;
-        document.getElementById("titulo").style.color="black"
+        document.getElementById("titulo").style.color = "black"
     }
 
 }
