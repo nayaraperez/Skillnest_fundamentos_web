ff29cb9414a7356b2dd6d088725bc0a08b0ab5a5
nayaraperez <nayaraperez@liceovvh.cl>
2026-07-13 15:15:13 -0400

    ds

---
+++ STACK_JS/04_bucles/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexión de js exitosa...");
+
+/*Ejemplo Bucle for*/
+function contarHasta2() {
+    for (let i = 0; i <3; i++) {
+    alert(i);
+    }
+}
+
+/*Ejemplo Bucle  while*/ 
