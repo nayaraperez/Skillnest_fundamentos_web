437c98fe5dc90d91f2dc0bacfdfcd698e951ebf6
nayaraperez <nayaraperez@liceovvh.cl>
2026-07-14 11:15:32 -0400

    lñk

--- STACK_JS/04_bucles/static/js/script.js
+++ STACK_JS/04_bucles/static/js/script.js
@@ -1,10 +1,74 @@
-console.log("conexión de js exitosa...");
+console.log("Prueba de conexion Js...");
 
 /*Ejemplo Bucle for*/
 function contarHasta2() {
-    for (let i = 0; i <3; i++) {
-    alert(i);
+    for (let i = 1; i < 5; i++) {
+        alert(i);
     }
 }
 
-/*Ejemplo Bucle  while*/ 
+
+
+function cuentaRegresiva() {
+    for (let i = 10; i > 0; i--) {
+        alert(i);
+    }
+}
+
+function recorrerLista() {
+    //Lista es un tipo de dato (array en ingles, lista en español y arreglos son lo mismo) 
+    //[]=["nombre", 4, True, [2,4]]
+    /*
+    let nombre ="Carlos;
+    alert(nombre);
+                        0        1
+    let nombres =   ["Carlos","Maria"]
+    alert (nombres[0]) -->Imprime Carlos
+    alert(nombres[1]) --> Imprime María
+    alert(nombres[0].length); --> 6
+    alert(nombres.length); --> 2   //nombres.length= cuenta cuantos elementos tienen la variable
+    */
+    let usuarios = ["Ana", "Luis", "María", "Nicolas", "Cleo"];
+
+    for (let i = 0; i < usuarios.length; i++) {
+        alert("Bienvenido, " + usuarios[i]);
+    }
+}
+
+
+/*Ejemplo Bucle while*/
+
+function reproducirCancion() {
+    let reproduciendo = true;
+
+    while (reproduciendo) {
+        alert("La canción sigue sonando...");
+        // Simulación de una acción que detendría la reproducción
+        reproduciendo = false;
+    }
+}
+
+function CargarDatos() {
+    let datosPendientes = 5;
+
+    while (datosPendientes > 0) {
+        alert(`Cargando datos...${datosPendientes} de 5`);
+        datosPendientes--;
+    }
+}
+
+function validarContrasena (){
+    let contraseñaCorrecta = false;
+//   !variable= variable es distinta a verdadera - dentro de condicion
+    while (!contraseñaCorrecta) {
+        let intento = prompt("Ingresa tu contraseña:");
+        if (intento === "1234") {
+            contraseñaCorrecta = true;
+            alert("Acceso concedido.");
+            break; //rompe el bucle
+            alert("no se ejecuta")
+        } else {
+            alert("Contraseña incorrecta, intenta de nuevo.");
+        }
+    }
+}
