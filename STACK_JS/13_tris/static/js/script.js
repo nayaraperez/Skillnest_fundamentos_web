1e257788f4885df7c35b0c322dcc56a89d5ff9c7
nayaraperez <nayaraperez@liceovvh.cl>
2026-08-10 09:30:23 -0400

    ok

--- STACK_JS/13_tris/static/js/script.js
+++ STACK_JS/13_tris/static/js/script.js
@@ -3,11 +3,27 @@ let boton = document.getElementById("cambiarTexto");
 
 // Agregamos un evento 'click' al botón
 boton.addEventListener("click", function () {
-        let textoBoton = boton.textContent;
-        if (textoBoton == "Haz click en mí"){
-            this.innerText ="¡Texto cambiado!";
-        }else {
-            this.innerText = "Haz click en mí";
-        }
-    
+    let textoBoton = boton.textContent;
+    if (textoBoton == "Haz click en mí") {
+        this.innerText = "¡Texto cambiado!";
+    } else {
+        this.innerText = "Haz click en mí";
+    }
+
+});
+
+
+
+
+
+
+// Seleccionamos todas las imágenes con la clase 'imagenEliminar'
+let imagenes = document.querySelectorAll(".imagenEliminar");
+
+// Agregamos un evento 'click' a cada imagen
+imagenes.forEach(function (imagen) {
+    imagen.addEventListener("click", function () {
+        // Eliminamos la imagen al hacer clic
+        this.remove();
+    });
 });
---
+++ STACK_JS/14_Evento_Onlick_(Core)/static/js/script.js
@@ -0,0 +1,1 @@
+console.log("conexión de js exitosa...");
