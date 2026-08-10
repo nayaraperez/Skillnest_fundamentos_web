4c1010db9c2600ec28f98c418b7eff000f135932
nayaraperez <nayaraperez@liceovvh.cl>
2026-08-10 15:15:55 -0400

    12

--- STACK_JS/14_Evento_Onlick_(Core)/static/js/script.js
+++ STACK_JS/14_Evento_Onlick_(Core)/static/js/script.js
@@ -1,1 +1,28 @@
 console.log("conexión de js exitosa...");
+
+// Seleccionamos el botón por su id
+let boton = document.getElementById("cambiarTexto");
+
+// Agregamos un evento 'click' al botón
+boton.addEventListener("click", function () {
+    let textoBoton = boton.textContent;
+    if (textoBoton == "Iniciar sesión") {
+        this.innerText = "Cerrar sesión";
+    } else {
+        this.innerText = "Iniciar sesión";
+    }
+
+});
+
+
+
+
+function Ver_Perfil(){
+    alert(`Bienvenido a tu perfil`);
+}
+
+function contador() {
+    for (let i = 1; i <= 10; i++) {
+        alert(`interaccion ${i}`)
+    }
+}
