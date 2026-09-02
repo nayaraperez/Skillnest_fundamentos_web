55cea7f63a38491e04ec84635709a17a68567c14
nayaraperez <nayaraperez@liceovvh.cl>
2026-09-02 10:42:15 -0400

    ok

--- STACK_JS/18_Me_gusta/static/js/script.js
+++ STACK_JS/18_Me_gusta/static/js/script.js
@@ -1,1 +1,16 @@
-console.log("conexión exitosa...");
+
+const publicaciones = document.querySelectorAll('.cajas');
+
+publicaciones.forEach((publicacion) => {
+
+    const boton = publicacion.querySelector('.botones_likes');
+    const contador = publicacion.querySelector('.contador');
+
+    boton.onclick = () => {
+
+        let likes = parseInt(contador.innerText);
+        likes++;
+
+        contador.innerText = `${likes} like(s)`;
+    };
+});
