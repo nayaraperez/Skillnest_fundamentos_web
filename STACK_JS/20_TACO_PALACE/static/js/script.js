b0122a722ee607ff30edd1d431638ba15168b2de
Naya <nayaraperez@liceovvh.cl>
2026-09-06 23:03:33 -0300

    ok

--- STACK_JS/20_TACO_PALACE/static/js/script.js
+++ STACK_JS/20_TACO_PALACE/static/js/script.js
@@ -1,16 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-const publicaciones = document.querySelectorAll('.cajas');
+    // 1. AUMENTAR CONTADOR DEL CARRITO (+1)
+    const botonesMas = document.querySelectorAll('.Mas');
+    const contadorCarrito = document.querySelector('.carrito_cero span');
+    let cantidad = 0;
 
-publicaciones.forEach((publicacion) => {
+    botonesMas.forEach(boton => {
+        boton.addEventListener('click', () => {
+            cantidad++;
+            contadorCarrito.textContent = cantidad;
+        });
+    });
 
-    const boton = publicacion.querySelector('.botones_likes');
-    const contador = publicacion.querySelector('.contador');
+    // 2. ALERTA DE LOGIN
+const inputEmail = document.getElementById('usuario');
+    const botonLogin = document.querySelector('.boton_naranja');
 
-    boton.onclick = () => {
+    botonLogin.addEventListener('click', () => {
+        const correo = inputEmail.value.trim();
 
-        let likes = parseInt(contador.innerText);
-        likes++;
+        if (correo === '') {
+            alert('Por favor, ingresa un correo electrónico.');
+        } else {
+            alert(`Bienveni@: ${correo}`);
+            inputEmail.value = '';
+        }
+    });
 
-        contador.innerText = `${likes} like(s)`;
-    };
+    // 3. CAMBIAR IMAGEN AL PASAR EL CURSOR (HOVER)
+    const imagenBanner = document.querySelector('.section_izquerda img');
+    const imagenOriginal = imagenBanner.src;
+
+
+
+    // Cambia a 'comida-mexicana2.jpg' al pasar el cursor por la imagen del banner
+    const imagenSecundaria = 'static/img/Imágenes/comida-mexicana2.jpg';
+
+    imagenBanner.addEventListener('mouseenter', () => {
+        imagenBanner.src = imagenSecundaria;
+    });
+
+    imagenBanner.addEventListener('mouseleave', () => {
+        imagenBanner.src = imagenOriginal;
+    });
+
 });
